f8016061b2b6ba42eaea26fc3968c1436eed0439
jwillesen <git@wizardwell.net>
2021-06-10 13:34:12 -0600

    Generate grid of cards with dealing animation

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from "react"
 import Card from "./Card"
+import { nRows, nCols, dealAnimationDelay } from "../constants"
+
+import styles from "./Grid.module.css"
+
+const cardIndexes: number[] = []
+for (let i = 0; i < nRows * nCols; ++i) {
+  cardIndexes.push(i)
+}
+
+const cardValues = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
 
 export default function Grid() {
+  const [animationIndex, setAnimationIndex] = useState(0)
+
+  useEffect(() => {
+    if (animationIndex >= cardIndexes.length) return
+    setTimeout(() => setAnimationIndex(i => i + 1), dealAnimationDelay)
+  }, [animationIndex])
+
   return (
-    <div>
-      <Card value="A" row={0} col={0} />
-      <Card value="B" row={1} col={1} />
-      <Card value="C" row={2} col={2} />
+    <div className={styles.grid}>
+      {cardIndexes.map(index => {
+        return (
+          <Card
+            key={index}
+            value={cardValues[Math.floor(index / 2)]}
+            row={animationIndex > index ? Math.floor(index / nCols) + 1 : 0}
+            col={animationIndex > index ? Math.floor(index % nCols) : 0}
+          />
+        )
+      })}
     </div>
   )
 }
---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const nRows = 4
+export const nCols = 6
+
+export const dealAnimationDelay = 200
