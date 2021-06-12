2734d625f05077ea55b6488092a1edcb699a16e9
jwillesen <git@wizardwell.net>
2021-06-12 10:22:50 -0600

    shuffle cards at game start

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react"
+import _shuffle from "lodash/shuffle"
 import { store } from "./pullstate"
 import Header from "./components/Header"
 import Grid from "./components/Grid"
@@ -12,6 +13,9 @@ function App() {
 
   // Really only want this to run this effect once when the page loads
   useEffect(() => {
+    store.update(s => {
+      s.cards = _shuffle(s.cards)
+    })
     gameState.startGame()
   }, []) // eslint-disable-line react-hooks/exhaustive-deps
 
